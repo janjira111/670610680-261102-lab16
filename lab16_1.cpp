c9e1e3102f0e0c65e5acf2bc9751527d32d70a34
janjira111 <janjira_thanach@cmu.ac.th>
2025-01-31 17:22:33 +0700

    completed

--- lab16_1.cpp
+++ lab16_1.cpp
@@ -32,3 +32,42 @@ int main(){
 	findColSum(dPtr,sum2,N,M); 
 	showData(sum2,1,M);
 }
+
+void showData(double * dPtr, int N, int M){
+    cout << fixed << setprecision(2);
+    for(int i = 0; i < N*M; i++){
+        cout << *(dPtr+i);
+        if(i%M==M-1) cout << endl;
+        else cout << " ";
+    }
+}
+
+void randData(double *dPtr,int N,int M){
+	    for(int i = 0; i < N*M; i++){
+        *(dPtr+i) = rand()%101/100.00;
+    }
+}
+
+void findRowSum(const double *dPtr,double *sum1,int N,int M){
+	double sum = 0;
+	int a =0;
+	for(int i = 0; i<N*M; i++){
+		sum += *(dPtr+i);
+		if(i%M==M-1){
+			*(sum1+a) = sum;
+			sum = 0;
+			a++;
+		}
+	}
+}
+
+void findColSum(const double *dPtr,double *sum2,int N,int M){
+	double sum = 0;
+	for(int j = 0; j<M; j++){
+		for(int i = 0; i<N; i++){
+			sum += *(dPtr+i*M+j);
+		}
+	*(sum2+j) = sum;
+	sum = 0;
+	}
+}
